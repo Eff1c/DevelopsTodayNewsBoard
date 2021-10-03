f6196e7abb9178b277a249154c3677f742f80a06
eff1c <illymartynyk@gmail.com>
2021-10-03 20:29:51 +0300

    Add new tables

--- newsboard/newsboard/models.py
+++ newsboard/newsboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.URLField()
+    creation_date = models.DateField(auto_now_add=True)
+    amount_of_upvotes = models.IntegerField()
+    author_name = models.CharField(max_length=100)
+
+class Comment(models.Model):
+    author_name = models.CharField(max_length=100)
+    content = models.TextField()
+    creation_date = models.DateField(auto_now_add=True)
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name="comments",)
