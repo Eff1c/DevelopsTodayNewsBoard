bb337f586c8ab4a5175180c3bb3d76823e76ccd8
eff1c <illymartynyk@gmail.com>
2021-10-04 15:36:38 +0300

    Add defaul param for amount_of_upvotes

--- newsboard/newsboard/models.py
+++ newsboard/newsboard/models.py
@@ -5,8 +5,11 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     link = models.URLField()
     creation_date = models.DateField(auto_now_add=True)
-    amount_of_upvotes = models.IntegerField()
+    amount_of_upvotes = models.IntegerField(default=0)
     author_name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["-amount_of_upvotes"]
 
 class Comment(models.Model):
     author_name = models.CharField(max_length=100)
@@ -15,3 +18,6 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,
                              on_delete=models.CASCADE,
                              related_name="comments",)
+
+    class Meta:
+        ordering = ["-creation_date"]
